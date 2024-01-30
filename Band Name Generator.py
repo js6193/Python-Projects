1062eb62debcfb9c0f4c86ac63177b2c4e11739f
js6193 <156113891+js6193@users.noreply.github.com>
2024-01-31 02:47:54 +0530

    Vs Code
    Its a band Name Generator which gives a simple name for your Band.

---
+++ Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome To The Band Name Generator")
+a = input("Whats the Name of the city you grew up in \n")
+b = input("Whats Your Pet name  \n")
+print("Your Band Name Could Be "  + a + " " + b )
